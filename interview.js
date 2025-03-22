2f8bf47922d87e4fa10eeb4f4fbf47feaccb2bde
Theo Zhang <theozhang722@gmail.com>
2025-03-22 21:21:13 +0800

    Update interview.js

--- interview.js
+++ interview.js
@@ -1,11 +1,12 @@
 function downloadVideo() {
-    
+   
     const link = document.createElement('a');
-    link.href = 'https://github.com/iamtheozzz/video/releases/download/untagged-2e869fe77fd4f3a43215/interview.mov';
+    link.href = 'https://github.com/iamtheozzz/video/releases/download/interview/interview.mov';
     link.download = 'interview.mov';
     
-    
+
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 }
+
